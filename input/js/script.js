34d19e5e35bbdfff3e39b04468994d7bbb81b20d
Abid Al-Wasi <abidalwasi@outlook.com>
2020-12-07 23:48:47 +0600

    updated project

--- input/js/script.js
+++ input/js/script.js
@@ -6,14 +6,14 @@ window.onload = function () {
   const inputEmail = document.querySelector(".inputEmail");
   
   inputName.addEventListener("blur", (e) => {
-    inputName.style.color = "#6d6d6d";
-    console.log("blurred")
+    // inputName.style.color = "#6d6d6d";
+    console.log("1");
     e.preventDefault();
   });
   
   inputEmail.addEventListener("blur", (e) => {
-    inputEmail.style.color = "#6d6d6d";
-    console.log("blurred")``
+    // inputEmail.style.color = "#6d6d6d";
+    console.log("2");
     e.preventDefault();
   });
 }
