705e60847d80885cfd94a5c60a0c33539b82a1a7
Abid Al-Wasi <abidalwasi@outlook.com>
2020-12-13 04:38:45 +0600

    updated project

--- input/js/script.js
+++ input/js/script.js
@@ -1,6 +1,10 @@
 window.onload = function () {
-  const name = document.getElementById("username");
+  const form = document.getElementById("form");
+  const username = document.getElementById("username");
   const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  const password2 = document.getElementById("password2");
+  
   const signUpForm = document.getElementById("formSignUp");
   const inputName = document.querySelector(".inputName");
   const inputEmail = document.querySelector(".inputEmail");
@@ -8,14 +12,88 @@ window.onload = function () {
   const inputConfirmPassword = document.querySelector(".inputConfirmPassword");
   const errorMessage = document.querySelectorAll(".errorMessage");
   
+  
   inputName.addEventListener("blur", inputLeave);
   inputEmail.addEventListener("blur", inputLeave);
   inputPassword.addEventListener("blur", inputLeave);
   inputConfirmPassword.addEventListener("blur", inputLeave);
   
   function inputLeave(e) {
-    console.log("left")
-    // if ()
     e.preventDefault();
+    console.log("left")
+    checkInputs();
   }
+  
+  // form.addEventListener("submit", e => {
+  //   e.preventDefault();
+  //
+  //   checkInputs();
+  // });
+  
+  function checkInputs() {
+    // console.log("left")
+    // trim to remove the whitespaces
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+    
+    if(usernameValue === "") {
+      setErrorFor(username, "Username cannot be blank");
+    } else {
+      setSuccessFor(username);
+    }
+    
+    if(emailValue === "") {
+      setErrorFor(email, "Email cannot be blank");
+    } else if (!isEmail(emailValue)) {
+      setErrorFor(email, "Not a valid email");
+    } else {
+      setSuccessFor(email);
+    }
+    
+    if(passwordValue === "") {
+      setErrorFor(password, "Password cannot be blank");
+    } else {
+      setSuccessFor(password);
+    }
+    
+    if(password2Value === "") {
+      setErrorFor(password2, "Password2 cannot be blank");
+    } else if(passwordValue !== password2Value) {
+      setErrorFor(password2, "Passwords does not match");
+    } else{
+      setSuccessFor(password2);
+    }
+  }
+  
+  function setErrorFor(input, message) {
+    const formControl = input.parentElement.parentElement;
+    formControl.className = "form-control error";
+    // const small = formControl.querySelector("small");
+    // small.innerText = message;
+  }
+  
+  function setSuccessFor(input) {
+    const formControl = input.parentElement.parentElement;
+    formControl.className = "form-control success";
+  }
+  
+  function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+  }
+  
+// SOCIAL PANEL JS
+//   const floating_btn = document.querySelector(".floating-btn");
+//   const close_btn = document.querySelector(".close-btn");
+//   const social_panel_container = document.querySelector(".social-panel-container");
+//
+//   floating_btn.addEventListener("click", () => {
+//     social_panel_container.classList.toggle("visible")
+//   });
+//
+//   close_btn.addEventListener("click", () => {
+//     social_panel_container.classList.remove("visible")
+//   });
+  
 }
