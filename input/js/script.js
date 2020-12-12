86022068cd74e89dd574de9a51703463ccafdfdf
Abid Al-Wasi <abidalwasi@outlook.com>
2020-12-13 04:44:57 +0600

    updated project

--- input/js/script.js
+++ input/js/script.js
@@ -24,15 +24,9 @@ window.onload = function () {
     checkInputs();
   }
   
-  // form.addEventListener("submit", e => {
-  //   e.preventDefault();
-  //
-  //   checkInputs();
-  // });
   
   function checkInputs() {
-    // console.log("left")
-    // trim to remove the whitespaces
+    
     const usernameValue = username.value.trim();
     const emailValue = email.value.trim();
     const passwordValue = password.value.trim();
@@ -70,8 +64,6 @@ window.onload = function () {
   function setErrorFor(input, message) {
     const formControl = input.parentElement.parentElement;
     formControl.className = "form-control error";
-    // const small = formControl.querySelector("small");
-    // small.innerText = message;
   }
   
   function setSuccessFor(input) {
@@ -82,18 +74,5 @@ window.onload = function () {
   function isEmail(email) {
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
   }
-  
-// SOCIAL PANEL JS
-//   const floating_btn = document.querySelector(".floating-btn");
-//   const close_btn = document.querySelector(".close-btn");
-//   const social_panel_container = document.querySelector(".social-panel-container");
-//
-//   floating_btn.addEventListener("click", () => {
-//     social_panel_container.classList.toggle("visible")
-//   });
-//
-//   close_btn.addEventListener("click", () => {
-//     social_panel_container.classList.remove("visible")
-//   });
   
 }
